1447dc3798c8fea861bf73683bcf04f329c0e1d9
Lucky Kadam <luckykadam94@gmail.com>
2021-01-19 21:16:48 +0530

    update make-dist.sh

--- make-dist.sh
+++ make-dist.sh
@@ -7,4 +7,4 @@ fi
 echo "preparing release $1"
 
 addon_zip="Render Strip $1 - Do not unzip!.zip"
-zip -r "$addon_zip" render-strip
+zip -r "$addon_zip" render-strip -x '*/.git/*' -x '*/.DS_Store/*' -x '*/__pycache__/*'
