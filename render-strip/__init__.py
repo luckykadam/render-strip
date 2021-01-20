d30cd4ddb7cb027e2ab9df4751f22596fbc2518c
Lucky Kadam <luckykadam94@gmail.com>
2021-01-20 19:40:41 +0530

    update version to 0.3.0

--- render-strip/__init__.py
+++ render-strip/__init__.py
@@ -8,7 +8,7 @@ bl_info = {
     "category": "Render",
     "blender": (2, 80, 0),
     "author" : "Lucky Kadam <luckykadam94@gmail.com>",
-    "version" : (0, 2, 0),
+    "version" : (0, 3, 0),
     "description" : "Render camera strips",
 }
 
